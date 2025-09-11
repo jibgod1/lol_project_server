ab8e915b48d6fc4e15e6f74b0eca8fbb64e39339
jibgod1 <jibgod@naver.com>
2025-09-11 18:37:22 +0900

    env update

--- app/config.py
+++ app/config.py
@@ -1,12 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), "api_key.env")
+load_dotenv(dotenv_path)
+
 
 API_KEY = os.getenv("API_KEY")
 
 if not API_KEY:
     raise ValueError("⚠️ Riot API Key가 설정되지 않았습니다. .env 파일을 확인하세요.")
+
+# %%
+
+
+
+
 
 # %%
 
